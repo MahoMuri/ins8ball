92ece9d88f25702a510f012c4afdbdbb667f7b8b
MahoMuri <paolo.santos_12@yahoo.com.ph>
2023-01-07 19:45:27 +0800

    added pages

---
+++ pages/compile.tsx
@@ -0,0 +1,8 @@
+import Discord from "../assets/img/icon-discord.svg";
+import Email from "../assets/img/icon-email.svg";
+import Facebook from "../assets/img/icon-facebook.svg";
+import Instagram from "../assets/img/icon-instagram.svg";
+
+export default function Compile() {
+  return <></>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,32 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Ball from "../assets/img/ball.svg";
+import { Icon } from "@iconify-icon/react";
 
 export default function Home() {
+  const sendAlert = () => {
+    alert("This works");
+  };
   return (
     <>
       <Head>
         <title>ins8ball</title>
       </Head>
-      <div className="flex justify-center">
+      <div className="flex flex-col gap-7 pt-3 pb-10 items-center">
         <Ball />
+        <div className={styles.inputField}>
+          <input
+            type="text"
+            className={styles.inputBox}
+            placeholder="tell me what you think?"
+          />
+          <Icon
+            icon="majesticons:send"
+            width={66}
+            className={styles.sendBtn}
+            onClick={sendAlert}
+          />
+        </div>
       </div>
     </>
   );
